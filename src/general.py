79a7e12fe34d996d23cc060393028978449ba578
Ravi Teja <raviteja2792+github@gmail.com>
2016-12-24 23:13:03 +0530

    First Commit!!

--- src/general.py
+++ src/general.py
@@ -3,7 +3,7 @@ import os
 # Each website is an seperate project
 def create_project_dir(directory):
     if not os.path.exists(directory):
-        print("Creating project" +directory)
+        print("Creating project" + directory)
         os.makedirs(directory)
 
-create_project_dir('thedirectory')
+create_project_dir('thenewboston')
