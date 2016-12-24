1ef2cac5eddd1560b5710007bf3c99e009fca1e0
Ravi Teja <raviteja2792+github@gmail.com>
2016-12-24 23:34:21 +0530

    First Commit!!

--- src/general.py
+++ src/general.py
@@ -1,9 +1,23 @@
 import os
 
-# Each website is an seperate project
-def create_project_dir(directory):
+# Each website is an separate project
+def  create_project_dir(directory):
     if not os.path.exists(directory):
         print("Creating project" + directory)
         os.makedirs(directory)
 
 create_project_dir('thenewboston')
+#Create queue and crawled files(if not created)
+def create_data_files(project_name, base_url):
+    queue = project_name + 'queue.txt'
+    crawled = project_name + 'crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+# Create a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
